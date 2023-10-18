ac95ccc76f6970901996f732b72a43c0714fa3cc
nisha1289 <147962898+nisha1289@users.noreply.github.com>
2023-10-18 16:44:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -104,83 +104,119 @@ async def account_login(bot: Client, m: Message):
         thumb = "thumb.jpg"
     else:
         thumb == "no"
-    if raw_text =='0':
-        count =1
-    else:       
-        count = int(raw_text)
-    for i in range(arg, len(links)):
-      try:
-        url = links[i][1]
-        name = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@","").replace("*","").replace(".","").strip()
-      except IndexError:
-        pass
-      command_to_exec = [
-              "yt-dlp",
-              "--no-warnings",
-              "--socket-timeout",
-              "30",
-              "-R",
-              "25",
-              url,
-              "--fragment-retries",
-              "25",
-              "--external-downloader",
-              "aria2c",
-              "--downloader-args",
-              "aria2c: -x 16 -j 32"
-          ]
-      if "youtu" in url:
-          ytf = f"b[height<={vid_format}][ext=mp4]/bv[height<={vid_format}][ext=mp4]+ba[ext=m4a]/b[ext=mp4]"
-          command_to_exec.extend(["-f",ytf,"-o",name+".%(ext)s", ])
-      elif ".m3u8" in url:
-        ytf = f"b[height<={vid_format}]/bv[height<={vid_format}]+ba"  
-        command_to_exec.extend(["-f",ytf,"-o",name+".%(ext)s", ])
-      elif ".mp4" in url:
-        ytf = f"b[height<={vid_format}]/bv[height<={vid_format}]+ba"  
-        command_to_exec.extend(["-f",ytf,"-o",name+".%(ext)s", ])
-      elif ".pdf" in url:
-          command_to_exec.extend(['yt-dlp','-o',f'{name}.pdf',url])
-      else:
-        ytf = f"{ytf}/b[height<={vid_format}]/bv[height<={vid_format}]+ba/b/bv+ba"  
-      command_to_exec.extend(["-f",ytf,"-o",name+".%(ext)s"])
-      Show = f"**Downloading**: __{name}__\n"
-      await exec(command_to_exec)
-      prog = await m.reply_text(Show)
-      if ".pdf" in url:
-          cc2 = f'{str(count).zfill(2)}. {name}\n\n**Batch »** {mm}\n**Dowloaded By »** {raw_text0}'
-          await bot.send_document(document = name+".pdf",caption=cc2)
-          os.remove(f"{name}")
-          count+=1
-      try:
-        if thumb == "no":
-            thumbnail = f"{name}.jpg"
-        else:
-            thumbnail = "thumb.jpg"
-      except Exception as e:
+         
+           
+    count =1    
+    try:
+        for i in range(arg, len(links)):
+        
+            url = links[i][1]
+            name = links[i][0].replace("\t", "")
+                # await m.reply_text(name +":"+ url)
+
+            Show = f"**Downloading:-**\n\n**Name :-** ```{name}\nQuality - {raw_text2}```\n\n**Url :-** ```{url}```"
+            prog = await m.reply_text(Show)
+            cc = f'>> **Name :** {name}\n>> **Title :** {raw_text0}\n\n>> **Index :** {count}'
+
+            if "youtu" in url:
+                if raw_text2 in ["144", "240", "480"]:
+                    ytf = f'bestvideo[height<={raw_text2}][ext=mp4]+bestaudio[ext=m4a]'
+                elif raw_text2 == "360":
+                    ytf = 18
+                elif raw_text2 == "720":
+                    ytf = 22
+                else:
+                    ytf = 18
+            else:
+                ytf=f"bestvideo[height<={raw_text2}]"
+
+            if ytf == f'bestvideo[height<={raw_text2}][ext=mp4]+bestaudio[ext=m4a]':
+                cmd = f'yt-dlp -o "{name}.mp4" -f "{ytf}" "{url}"'
+            elif raw_text2 == "no":
+                cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+            elif "jwplayer" in url:# and raw_text2 in ["144", "240", "360", "480", "720", "no"]:
+                cmd=f'yt-dlp -o "{name}.mp4" "{url}"'    
+            elif "adda" in url:# and raw_text2 in ["144", "240", "360", "480", "720", "no"]:
+                cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+            elif ".pdf" in url: #and raw_text2 in ["144", "240", "360", "480", "720", "no"]:
+                cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
+            elif "drive" in url:
+                cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
+            else:
+                cmd = f'yt-dlp -o "{name}.mp4" -f "{ytf}+bestaudio" "{url}"'
+
+            
+                # download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
+                # os.system(download_cmd)
+            try:
+                if cmd == f'yt-dlp -o "{name}.pdf" "{url}"' or ".pdf" in url:
+                    ka=await helper.download(url)
+                    if timer.can_send():
+                        async def progress_bar(current,total):
+                            try:
+                                await reply.edit(f"{current * 100 / total:.1f}%")
+                            except FloodWait as e:
+                                time.sleep(e.x)
+                    reply = await message.reply("Uploading")
+                    start_time = time.time()
+
+                    await m.reply_document(ka, caption=f'>> **File :** {name}\n>> **Title :** {raw_text0}\n\n>> **Index :** {count}',progress=progress_bar,progress_args=(reply,start_time))
+                    count+=1
+                    await prog.delete (True)
+                    os.remove(ka)
+
+                    # filename = f"{name}.pdf"
+                    # await m.reply_document(f'{name}.pdf',caption =f'>> **File :** {name}\n>> **Title :** {raw_text0}\n\n>> **Index :** {count}')
+                    # count+=1
+                    # await prog.delete (True)
+                    # os.remove(f'{name}.pdf')
+                    # time.sleep(2)
+                else:
+                    try:
+                        download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
+                        os.system(download_cmd)
+                        filename = f"{name}.mp4"
+                        subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
+                        await prog.delete (True)
+                        if timer.can_send():
+                            async def progress_bar(current,total):
+                                try:
+                                    await reply.edit(f"{current * 100 / total:.1f}%")
+                                except FloodWait as e:
+                                    time.sleep(e.x)
+                        reply = await m.reply_text(f"Uploading - ```{name}```")
+                        try:
+                            if thumb == "no":
+                                thumbnail = f"{filename}.jpg"
+                            else:
+                                thumbnail = thumb
+                        except Exception as e:
+                            await m.reply_text(str(e))
+
+                        dur = int(helper.duration(filename))
+            #                         await prog.delete (True)
+                        start_time = time.time()
+                        await m.reply_video(f"{name}.mp4",caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur, progress=progress_bar,progress_args=(reply,start_time))
+                        count+=1
+                        os.remove(f"{name}.mp4")
+
+                        os.remove(f"{filename}.jpg")
+                        await reply.delete (True)
+
+                    except Exception as e:
+                        await m.reply_text(f"**downloading failed ❌**\n{str(e)}\n**Name** - {name}\n**Link** - ```{url}```")
+                        continue
+            except Exception as e:
+                await m.reply_text(str(e))
+                continue
+    except Exception as e:
         await m.reply_text(str(e))
-        continue
-      else:
-        start_time = time.time()
-        cc = f'{str(count).zfill(2)}. {name} - {vid_format}p\n\n**Batch »** {mm}\n**Dowloaded By »** {raw_text0}'
-        try:
-          duration, width, height = get_video_attributes(path)
-        except:
-            duration = width = height = 0
-            pass
-        try:
-          await bot.send_video(
-              m.chat.id,
-              video=name+".mp4",
-              caption=cc,
-              duration=duration,
-              width=width,
-              height=height,
-              file_name=name,
-              supports_streaming=True)
-          count+=1    
-          await prog.delete (True)
-          os.remove(name+".mp4")
-        except:pass  
+    await m.reply_text("Done")
+
+
+
+
+
           
 
 @bot.on_message(filters.command(["cancel"]))
